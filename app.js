43a71d9227537597bcfe6dd313da588dc67756d9
Raul Oliva <raul.oliva@nxp.com>
2021-01-27 10:36:43 -0600

    Changes: * Implementing a third approach, process the links from the images.csv file and creating an spreedsheet with the special words found along with the image location

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const fs = require('fs')
 const xlsx = require('xlsx')
 var matrizWords = []
 const matriz = []
+var readWholePhrase = true
+
+const getWords = () => {
+    const data = fs.readFileSync('words.csv', {encoding: 'utf-8'})
+    return data.split(',')
+}
 
 const getArrayWords = text => {
-    const cell = text.description
+    const cell = text.description.toLowerCase()
     matrizWords.push(cell)
 }
 
 const exportXLSX = () => {
+    console.log(matriz);
     // print the words into a spredsheet
     const workbook = xlsx.utils.book_new()
     const worksheet = xlsx.utils.aoa_to_sheet(matriz)
@@ -20,6 +27,21 @@ const exportXLSX = () => {
     const unique = `${date.getDay()}_${date.getMonth()}_${date.getFullYear()}_${date.getHours()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`
     xlsx.writeFile(workbook, `reports/Report_${unique}.xlsx`)
     console.log('Finished!');
+}
+
+const detectSpecialWords = imgPath => {
+    const ocurrencies = matrizWords.filter(foundWord => {
+        const masterOcurrencies = (foundWord.match(/master/g) || [])
+        const slaveOcurrencies = (foundWord.match(/slave/g) || [])
+        const slavesOcurrencies = (foundWord.match(/slaves/g) || [])
+
+        const ocurrencies = [...masterOcurrencies, ...slaveOcurrencies, ...slavesOcurrencies]
+        if(ocurrencies.length > 0) {
+            return ocurrencies
+        }
+    })
+    ocurrencies[0] = imgPath
+    return ocurrencies
 }
 
 const detectText = () => {
@@ -37,8 +59,11 @@ const detectText = () => {
             const [result] = await client.textDetection(image);
             const detections = result.textAnnotations;
             detections.forEach(getArrayWords);
+            matrizWords = detectSpecialWords(image)
+
             matriz.push(matrizWords)
             matrizWords = []
+            readWholePhrase = true
         }
         resolve()
     })
