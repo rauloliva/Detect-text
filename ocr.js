314a93da4fe60267091ff5ee8e53c292d4d2f80f
Raul Oliva <raul.oliva@nxp.com>
2021-01-28 15:36:04 -0600

    Changes: * Fourth approach * Implementing a view and displaying in it the results in a table * Express, ejs and nodemon were neeeded for this purpose

--- ocr.js
+++ ocr.js
@@ -1,41 +1,23 @@
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 const fs = require('fs')
-const xlsx = require('xlsx')
 var matrizWords = []
-const matriz = [['Image URL', 'Found']]
-
-const getWords = () => {
-    const data = fs.readFileSync('words.csv', {encoding: 'utf-8'})
-    return data.split(',')
-}
+const matriz = []
 
 const getArrayWords = text => {
     const cell = text.description.toLowerCase()
     matrizWords.push(cell)
 }
 
-const exportXLSX = () => {
-    // print the words into a spredsheet
-    const workbook = xlsx.utils.book_new()
-    const worksheet = xlsx.utils.aoa_to_sheet(matriz)
-    xlsx.utils.book_append_sheet(workbook,worksheet,'Words')
-
-    const date = new Date()
-    const unique = `${date.getDay()}_${date.getMonth()}_${date.getFullYear()}_${date.getHours()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`
-    xlsx.writeFile(workbook, `reports/Report_${unique}.xlsx`)
-    console.log('Finished!');
-}
-
 const organizeCells = ocurrencies => {
     let size = ocurrencies.length
     var value = ''
     for (let i = 1; i < size; i++) {
-        value += `${ocurrencies[i]},`
+        value += `${ocurrencies[i]}, `
         ocurrencies[i] = null
     }
-    ocurrencies[1] = value.substring(0, value.length - 1)
-
+    //value.length - 2 in order to remove the last space and comma
+    ocurrencies[1] = value.trim().substring(0, value.length - 2)
     return ocurrencies
 }
 
@@ -63,7 +45,7 @@ const detectText = () => {
     // Creates a new client
     const client = new vision.ImageAnnotatorClient();
 
-    const promise = new Promise(async resolve => {
+    return new Promise(async resolve => {
         // Reads the text from each image
         for (const image of images) {
             // Performs text detection on the image file
@@ -75,9 +57,8 @@ const detectText = () => {
             matriz.push(matrizWords)
             matrizWords = []
         }
-        resolve()
+        resolve(matriz)
     })
-    promise.then(() => exportXLSX())
 }
 
-detectText()
+module.exports = detectText
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const detectText = require('./ocr')
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/views'))
+
+app.listen(port, () => console.log(`Server started at port: ${port}`))
+
+app.get('/', (req, res) => {
+    detectText().then(images => {
+        console.log("OCR has finished");
+        res.render('index', {images: images})
+    })
+})
